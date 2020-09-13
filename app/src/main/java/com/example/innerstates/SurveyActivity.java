32ff523b4bf1915b71344f54d84c4912a5213dfa
Mintra <kikist@gmail.com>
2020-09-13 22:43:52 +0900

    Add progress bar

--- app/src/main/java/com/example/innerstates/SurveyActivity.java
+++ app/src/main/java/com/example/innerstates/SurveyActivity.java
@@ -2,6 +2,7 @@ package com.example.innerstates;
 
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
@@ -12,10 +13,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.drawable.DrawableCompat;
 
+import android.util.Log;
 import android.util.Pair;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -25,6 +29,9 @@ import java.util.Map;
 
 public class SurveyActivity extends AppCompatActivity {
     private HashMap<String, Question[]> surveyQuestion = new HashMap<String, Question[]>();
+    private int currentPage = 1;
+    private LinearLayout questionLayOut;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,30 +39,64 @@ public class SurveyActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        questionLayOut = findViewById(R.id.questionLayout);
+
         createSurvey();
 
-        LinearLayout questionLayOut = (LinearLayout) findViewById(R.id.questionLayout);
+        displaySurvey();
 
-        Question questions[] = surveyQuestion.get("page1");
+
+
+    }
+
+    private void displaySurvey() {
+        String page = "page" + currentPage;
+        TextView pageTextView = findViewById(R.id.pageTextView);
+        pageTextView.setText(currentPage + " / 8");
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        
+        progressBar.setProgress((currentPage * 100) / 8);
+
+        Question questions[] = surveyQuestion.get(page);
         for (Question question: questions) {
             TextView textView = new TextView(this);
             textView.setPadding(0,10,0,0);
             textView.setText(question.getQuestionTitle());
             textView.setTextColor(Color.BLACK);
             textView.setGravity(Gravity.CENTER);
+            textView.setTypeface(null, Typeface.BOLD);
+
             questionLayOut.addView(textView);
             questionLayOut.addView(createRadioButton(question.getChoices()));
+
         }
 
+        Button nextButton = new Button(this);
+        nextButton.setText("Next");
+        nextButton.setId(Integer.parseInt(page.substring(page.length()-1)));
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("tafftaff----", v.getId()+"");
+                nextSurvey();
+            }
+        });
+        questionLayOut.addView(nextButton);
+    }
 
-
+    private void nextSurvey() {
+        questionLayOut.removeAllViews();
+        currentPage += 1;
+        displaySurvey();
     }
 
     private void createSurvey() {
         Question socialCompare1 = new Question("I was paying a lot of attention to how I do things compared to how people I follow on Instagram do things", disToAgree());
         Question socialCompare2 = new Question("I was wanting to find out how well I do things compared to people I follow on Instagram", disToAgree());
+
+        Question envy1 = new Question("I was generally feel inferior to others", disToAgree());
         surveyQuestion.put("page1", new Question[] {socialCompare1, socialCompare2});
-        surveyQuestion.put("page2", new Question[] {socialCompare1, socialCompare2});
+        surveyQuestion.put("page2", new Question[] {envy1, socialCompare2});
     }
 
     private RadioGroup createRadioButton(Choice[] choices) {
@@ -63,11 +104,13 @@ public class SurveyActivity extends AppCompatActivity {
         final RadioButton[] rb = new RadioButton[choices.length];
         RadioGroup rg = new RadioGroup(this); //create the RadioGroup
         rg.setOrientation(RadioGroup.HORIZONTAL);//or RadioGroup.VERTICAL
+        rg.setPadding(0,0,0,50);
         for (int i = 0; i < choices.length; i++) {
             rb[i] = new RadioButton(this);
             rb[i].setText(choices[i].getChoiceTitle());
             rb[i].setId(choices[i].getChoiceValue() + 100);
             rb[i].setButtonDrawable(null);
+            rb[i].setWidth(200);
 
             TypedArray a = getTheme().obtainStyledAttributes(R.style.AppTheme, new int[] {android.R.attr.listChoiceIndicatorSingle});
             int attributeResourceId = a.getResourceId(0, 0);
